cec62dad09161682e920a6d2a8c7a2eda02b4e3f
GUIHUFF <ghuff@alunos.utfpr.edu.br>
2022-06-17 20:22:26 -0300

    registrar pedido

--- src/Pages/RegistrationOrder/index.jsx
+++ src/Pages/RegistrationOrder/index.jsx
@@ -1,7 +1,64 @@
+import React,{ useState, useEffect} from "react";
 
+import "./styles.css";
+
+import Loading from "../../Components/Loading";
+import Header from "../../Components/Header";
 
 const RegistrationOrder = () => {
+  const [loading, setLoading] = useState(true);
+  
+  const [table, setTable] = useState("");
 
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  const handleSubmit = () => {}
+
+  if ( loading ) {
+    return(
+      <div className="container">
+        <div className="container-loading">
+          <div className="loading-container">
+            <Loading />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="container newOrder">
+        <div className="box-registration">
+          <div className="title">
+            <h2>Novo Pedido</h2>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="lineOne">
+              <label>Mesa:
+                <input id="tableNumber" type='text' required
+                value={table} 
+                onChange={(e) => setTable(e.target.value)} 
+                />
+              </label>
+              <label>Cliente:
+                <input type='text'
+                value={table} 
+                onChange={(e) => setTable(e.target.value)} 
+                />
+              </label>
+            </div>
+            <div className="boxItens">
+
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default RegistrationOrder;
